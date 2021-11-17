8380d2b392c9fecee536394021be196e5367c10c
Hugo <hugo.arenas@usach.cl>
2021-11-17 19:13:51 -0300

    Importaciones inecesarias

--- demo/src/main/java/com/alteabackend/demo/Repositories/PatologiaRepositoryImp.java
+++ demo/src/main/java/com/alteabackend/demo/Repositories/PatologiaRepositoryImp.java
@@ -1,7 +1,6 @@
 package com.alteabackend.demo.Repositories;
 
 import com.alteabackend.demo.Models.Patologia;
-import com.alteabackend.demo.Repositories.PatologiaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -10,17 +9,16 @@ import org.sql2o.Sql2o;
 
 import java.util.List;
 
-
 @Repository
 public class PatologiaRepositoryImp implements PatologiaRepository {
-    
+
     @Autowired
     private Sql2o sql2o;
-    
+
     @Override
     public int countPatologia() {
         int total = 0;
-        try(Connection conn = sql2o.open()){
+        try (Connection conn = sql2o.open()) {
             total = conn.createQuery("SELECT COUNT(*) FROM Patologia").executeScalar(Integer.class);
         }
         return total;
@@ -28,9 +26,12 @@ public class PatologiaRepositoryImp implements PatologiaRepository {
 
     @Override
     public List<Patologia> getAllPatologia() {
-        try(Connection conn = sql2o.open()){
-            return conn.createQuery("ID AS ID, Nombre AS Nombre, Descripcion AS Descripcion, Recomendacion AS Recomendacion").executeAndFetch(Patologia.class);
-        
+        try (Connection conn = sql2o.open()) {
+            return conn
+                    .createQuery(
+                            "ID AS ID, Nombre AS Nombre, Descripcion AS Descripcion, Recomendacion AS Recomendacion")
+                    .executeAndFetch(Patologia.class);
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
@@ -39,69 +40,61 @@ public class PatologiaRepositoryImp implements PatologiaRepository {
 
     @Override
     public Patologia createPatologia(Patologia Patologia) {
-        try(Connection conn = sql2o.open()){
-            Patologia v1 = conn.createQuery("select * from Patologia where Nombre=:Nombre").addParameter("Nombre",Patologia.getNombre()).executeAndFetchFirst(Patologia.class);
-            if (v1 == null){
-                long insertedId = countPatologia()+1;
-                conn.createQuery("insert into Patologia (ID, Nombre, Descripcion, Recomendacion)"+
-                        " values (:id, :PatologiaNombre, :PatologiaDescripcion, :PatologiaRecomendacion)") 
-                        .addParameter("id",  insertedId)                
-                        .addParameter("PatologiaNombre", Patologia.getNombre())
+        try (Connection conn = sql2o.open()) {
+            Patologia v1 = conn.createQuery("select * from Patologia where Nombre=:Nombre")
+                    .addParameter("Nombre", Patologia.getNombre()).executeAndFetchFirst(Patologia.class);
+            if (v1 == null) {
+                long insertedId = countPatologia() + 1;
+                conn.createQuery("insert into Patologia (ID, Nombre, Descripcion, Recomendacion)"
+                        + " values (:id, :PatologiaNombre, :PatologiaDescripcion, :PatologiaRecomendacion)")
+                        .addParameter("id", insertedId).addParameter("PatologiaNombre", Patologia.getNombre())
                         .addParameter("PatologiaDescripcion", Patologia.getDescripcion())
-                        .addParameter("PatologiaRecomendacion", Patologia.getRecomendacion())
-                        .executeUpdate().getKey();
+                        .addParameter("PatologiaRecomendacion", Patologia.getRecomendacion()).executeUpdate().getKey();
                 Patologia.setId(insertedId);
-                return Patologia;  
-            }else{
+                return Patologia;
+            } else {
                 return null;
             }
 
-      
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
         }
-        
+
     }
 
     @Override
-    public boolean deletePatologia(long id){
-        try(Connection conn = sql2o.open()){
-            conn.createQuery("DELETE FROM Patologia WHERE ID = :id")
-            .addParameter("id", id)
-            .executeUpdate();
-            return true; 
-        }catch(Exception e){
+    public boolean deletePatologia(long id) {
+        try (Connection conn = sql2o.open()) {
+            conn.createQuery("DELETE FROM Patologia WHERE ID = :id").addParameter("id", id).executeUpdate();
+            return true;
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return false;
         }
     }
 
     @Override
-    public Patologia getPatologia(long id){
-		String sql = "select ID AS ID, Nombre AS Nombre, Descripcion AS Descripcion, Recomendacion AS Recomendacion from Patologia where ID=:id";
-		try (Connection con = sql2o.open()) {
-			return con.createQuery(sql)
-				.addParameter("id", id)
-				.executeAndFetchFirst(Patologia.class);
-		}catch(Exception e){
+    public Patologia getPatologia(long id) {
+        String sql = "select ID AS ID, Nombre AS Nombre, Descripcion AS Descripcion, Recomendacion AS Recomendacion from Patologia where ID=:id";
+        try (Connection con = sql2o.open()) {
+            return con.createQuery(sql).addParameter("id", id).executeAndFetchFirst(Patologia.class);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
         }
-	}
+    }
 
     @Override
-    public boolean updatePatologia(Patologia nuevoPatologia){
+    public boolean updatePatologia(Patologia nuevoPatologia) {
         String updateSql = "update Patologia set Nombre = : Nombre, Descripcion = : Descripcion, Recomendacion = : Recomendacion where id = :id";
-        try (Connection con = sql2o.open()) {   
-            con.createQuery(updateSql)
-                .addParameter("Recomendacion", nuevoPatologia.getRecomendacion())
-                .addParameter("Descripcion", nuevoPatologia.getDescripcion())
-                .addParameter("Nombre",nuevoPatologia.getNombre())
-                .addParameter("id", nuevoPatologia.getId())
-                .executeUpdate();
+        try (Connection con = sql2o.open()) {
+            con.createQuery(updateSql).addParameter("Recomendacion", nuevoPatologia.getRecomendacion())
+                    .addParameter("Descripcion", nuevoPatologia.getDescripcion())
+                    .addParameter("Nombre", nuevoPatologia.getNombre()).addParameter("id", nuevoPatologia.getId())
+                    .executeUpdate();
             return true;
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return false;
         }
